90ae332ede37cb63aca84589e93b7ad9936ac28c
Arkadiusz Niemiec <arkadiusz.niemiec1@gmail.com>
2019-12-17 20:04:41 +0100

    transferring the project to git

---
+++ src/main/java/niemiec/SpringSecurityRestHibernateApplication.java
@@ -0,0 +1,13 @@
+package niemiec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRestHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityRestHibernateApplication.class, args);
+	}
+
+}
