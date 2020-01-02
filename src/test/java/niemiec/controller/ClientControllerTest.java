9bb2bf95ca689352cbea513a117b68410cc97680
niemczyk13 <arkadiusz.niemiec1@gmail.com>
2020-01-02 09:38:10 +0100

    Add tests in ClientControllerTest

--- src/test/java/niemiec/controller/ClientControllerTest.java
+++ src/test/java/niemiec/controller/ClientControllerTest.java
@@ -34,5 +34,50 @@ public class ClientControllerTest {
 				.param("endHour", "20:00"))
 				.andExpect(status().isOk());
 	}
+	
+	@Test
+	public void testReservationWithDateFromThePast() throws Exception {
+		clientControllerMock.perform(post("/reservation/new")
+				.param("name", "Karolina")
+				.param("surname", "Niemiec")
+				.param("email", "karka.niemiec@gmail.com")
+				.param("phoneNumber", "886618194")
+				.param("tableId", "1")
+				.param("numberOfPeople", "3")
+				.param("date", "2020-01-01")
+				.param("startHour", "19:00")
+				.param("endHour", "20:00"))
+				.andExpect(status().isBadRequest());
+	}
+	
+	@Test
+	public void testReservationWithBadEmail() throws Exception {
+		clientControllerMock.perform(post("/reservation/new")
+				.param("name", "Karolina")
+				.param("surname", "Niemiec")
+				.param("email", "karka.niemiecgmail.com")
+				.param("phoneNumber", "886618194")
+				.param("tableId", "1")
+				.param("numberOfPeople", "3")
+				.param("date", "2020-12-22")
+				.param("startHour", "19:00")
+				.param("endHour", "20:00"))
+				.andExpect(status().isBadRequest());
+	}
+	
+	@Test
+	public void testReservationWithBadPhoneNumber() throws Exception {
+		clientControllerMock.perform(post("/reservation/new")
+				.param("name", "Karolina")
+				.param("surname", "Niemiec")
+				.param("email", "karka.niemiec@gmail.com")
+				.param("phoneNumber", "88661814")
+				.param("tableId", "1")
+				.param("numberOfPeople", "3")
+				.param("date", "2020-12-22")
+				.param("startHour", "19:00")
+				.param("endHour", "20:00"))
+				.andExpect(status().isBadRequest());
+	}
 
 }
